22f51ce10d9ef295e34e5d0250b87852d436bdf3
Feng Wei <wei.feng@uisee.com>
2018-11-15 14:29:37 +0800

    vtracker-chart.rb add relocate loadmapchunk and misc
    Signed-off-by: Feng Wei <wei.feng@uisee.com>

--- script/vtracker-chart.rb
+++ script/vtracker-chart.rb
@@ -9,23 +9,26 @@
 ## Dependency:
 #               sudo apt-get install ruby
 #               sudo gem install gruff
+#               sudo gem install descriptive_statistics
 
 require 'gruff'
 require 'optparse'
 require 'ostruct'
+require 'descriptive_statistics'
 require 'time'
 
 
-VTRACKER_RECORD_LOAD_IMAGE = /Time for load_image\:\s([\d\.]+)ms/
-VTRACKER_RECORD_ORB = /Time for orb\: ([\d\.]+)ms/
-VTRACKER_RECORD_LOAD_CHUNK = /Time for Load Chunk \d+\:\s([\d\.]+)ms/
-VTRACKER_RECORD_TRACK_MOTION = /Time for track_motion\:\s([\d\.]+)ms/
-VTRACKER_RECORD_TRACK_LOCALMAP = /Time for track_localmap\:\s([\d\.]+)ms/
-VTRACKER_RECORD_UPDATE_LOCALMAP = /Time for update_localmap\:\s([\d\.]+)ms/
-VTRACKER_RECORD_SEARCH_MAPPOINTS = /Time for search_mappoints\:\s([\d\.]+)ms/
-VTRACKER_RECORD_LOCALMAP_POSE_OPT = /Time for localmap pose_optimize\:\s([\d\.]+)ms/
-VTRACKER_RECORD_STATE_OK = /\sstate\:\sOK$/
-VTRACKER_RECORD_STATE_LOST = /\sstate\:\sLOST$/
+VTRACKER_RECORD_LOAD_IMAGE = /^I(\d+\s[\d\:\.]+).*Time for load_image\:\s([\d\.]+)ms$/
+VTRACKER_RECORD_ORB = /^I(\d+\s[\d\:\.]+).*Time for orb\: ([\d\.]+)ms$/
+VTRACKER_RECORD_LOAD_CHUNK = /^I(\d+\s[\d\:\.]+).*Time for Load Chunk \d+\:\s([\d\.]+)ms$/
+VTRACKER_RECORD_RELOCATE = /^I(\d+\s[\d\:\.]+).*Time for relocate\:\s([\d\.]+)ms$/
+VTRACKER_RECORD_TRACK_MOTION = /^I(\d+\s[\d\:\.]+).*Time for track_motion\:\s([\d\.]+)ms$/
+VTRACKER_RECORD_TRACK_LOCALMAP = /^I(\d+\s[\d\:\.]+).*Time for track_localmap\:\s([\d\.]+)ms$/
+VTRACKER_RECORD_UPDATE_LOCALMAP = /^I(\d+\s[\d\:\.]+).*Time for update_localmap\:\s([\d\.]+)ms$/
+VTRACKER_RECORD_SEARCH_MAPPOINTS = /^I(\d+\s[\d\:\.]+).*Time for search_mappoints\:\s([\d\.]+)ms$/
+VTRACKER_RECORD_LOCALMAP_POSE_OPT = /^I(\d+\s[\d\:\.]+).*Time for localmap pose_optimize\:\s([\d\.]+)ms$/
+VTRACKER_RECORD_STATE_OK = /^I(\d+\s[\d\:\.]+).*state\:\sOK$/
+VTRACKER_RECORD_STATE_LOST = /^I(\d+\s[\d\:\.]+).*state\:\sLOST$/
 
 
 # --- Define Command Line Options --- #
@@ -70,25 +73,27 @@ def parse_tokenize(log_file)
     File.readlines(log_file).each do | line |
         begin
             if matched = line.match(VTRACKER_RECORD_LOAD_IMAGE)
-                record_stream << { :label => "load_image", :cost => matched[1].to_i }
+                record_stream << { :label => "load_image", :timestamp => Time.parse(matched[1]), :cost => matched[2].to_i }
             elsif matched = line.match(VTRACKER_RECORD_ORB)
-                record_stream << { :label => "orb", :cost => matched[1].to_i }
+                record_stream << { :label => "orb", :timestamp => Time.parse(matched[1]), :cost => matched[2].to_i }
             elsif matched = line.match(VTRACKER_RECORD_LOAD_CHUNK)
-                record_stream << { :label => "load_chunk", :cost => matched[1].to_i }
+                record_stream << { :label => "load_chunk", :timestamp => Time.parse(matched[1]), :cost => matched[2].to_i }
+            elsif matched = line.match(VTRACKER_RECORD_RELOCATE)
+                record_stream << { :label => "relocate", :timestamp => Time.parse(matched[1]), :cost => matched[2].to_i }
             elsif matched = line.match(VTRACKER_RECORD_TRACK_MOTION)
-                record_stream << { :label => "track_motion", :cost => matched[1].to_i }
+                record_stream << { :label => "track_motion", :timestamp => Time.parse(matched[1]), :cost => matched[2].to_i }
             elsif matched = line.match(VTRACKER_RECORD_TRACK_LOCALMAP)
-                record_stream << { :label => "track_localmap", :cost => matched[1].to_i }
+                record_stream << { :label => "track_localmap", :timestamp => Time.parse(matched[1]), :cost => matched[2].to_i }
             elsif matched = line.match(VTRACKER_RECORD_UPDATE_LOCALMAP)
-                record_stream << { :label => "update_localmap", :cost => matched[1].to_i }
+                record_stream << { :label => "update_localmap", :timestamp => Time.parse(matched[1]), :cost => matched[2].to_i }
             elsif matched = line.match(VTRACKER_RECORD_SEARCH_MAPPOINTS)
-                record_stream << { :label => "search_mappoints", :cost => matched[1].to_i }
+                record_stream << { :label => "search_mappoints", :timestamp => Time.parse(matched[1]), :cost => matched[2].to_i }
             elsif matched = line.match(VTRACKER_RECORD_LOCALMAP_POSE_OPT)
-                record_stream << { :label => "localmap_pose_opt", :cost => matched[1].to_i }
+                record_stream << { :label => "localmap_pose_opt", :timestamp => Time.parse(matched[1]), :cost => matched[2].to_i }
             elsif matched = line.match(VTRACKER_RECORD_STATE_OK)
-                record_stream << { :label => "ok", :cost => 0 }
+                record_stream << { :label => "ok", :timestamp => Time.parse(matched[1]), :cost => 0 }
             elsif matched = line.match(VTRACKER_RECORD_STATE_LOST)
-                record_stream << { :label => "lost", :cost => 0 }
+                record_stream << { :label => "lost", :timestamp => Time.parse(matched[1]), :cost => 0 }
             else
                 next
             end
@@ -117,11 +122,13 @@ end
 # Generate side effective to globals
 
 def process_group(group)
-    load_image, track_motion, update_localmap, search_mappoints, localmap_pose_opt = 0, 0, 0, 0, 0
+    load_image, track_motion, update_localmap, search_mappoints, localmap_pose_opt, relocate, load_mapchunk = 0, 0, 0, 0, 0, 0, 0
     state = group.pop
     raise "internal fatal error" if state.nil?
+    last_ts, first_ts = state[:timestamp], Time.now
     while not group.empty?
         rec = group.pop
+        first_ts = rec[:timestamp] - rec[:cost] / 1000.0
         case rec[:label]
         when "load_image"
             puts "ERROR: load_image more than once in group #{@group_count}" if load_image.nonzero?
@@ -133,19 +140,23 @@ def process_group(group)
             puts "ERROR: update_localmap more than once in group #{@group_count}" if update_localmap.nonzero?
             update_localmap = rec[:cost]
         when "search_mappoints"
-            puts "ERROR: search_mappoints more than once in group #{@group_count}" if search_mappoints.nonzero?
-            search_mappoints = rec[:cost]
+            search_mappoints += rec[:cost]
         when "localmap_pose_opt"
-            puts "ERROR: localmap_pose_opt more than once in group #{@group_count}" if localmap_pose_opt.nonzero?
-            localmap_pose_opt = rec[:cost]
+            localmap_pose_opt += rec[:cost]
+        when "relocate"
+            puts "ERROR: relocate more than once in group #{@group_count}" if relocate.nonzero?
+            relocate = rec[:cost]
+        when "load_chunk"
+            load_mapchunk += rec[:cost]
         when "orb"
             @orb_costs << rec[:cost]
-        when "load_chunk"
-            @load_chunk_costs << rec[:cost]
         end
     end
 
-    puts "[#{@group_count}]: #{state[:label]}, #{load_image}, #{track_motion}, #{update_localmap}, #{search_mappoints}, #{localmap_pose_opt}" if @options.verbose
+    misc = (last_ts - first_ts) * 1000 - load_image - track_motion - update_localmap - search_mappoints - localmap_pose_opt - relocate - load_mapchunk
+    misc = [misc.to_i, 0].max
+
+    puts "[#{@group_count}]: #{state[:label]}, #{load_image}, #{track_motion}, #{update_localmap}, #{search_mappoints}, #{localmap_pose_opt}, #{relocate}, #{load_mapchunk}, #{misc}" if @options.verbose
 
     @labels.merge!({ @group_count => if state[:label].eql? "ok" then 'V' else 'X' end })
     @datasets[0].last << load_image
@@ -153,6 +164,9 @@ def process_group(group)
     @datasets[2].last << update_localmap
     @datasets[3].last << search_mappoints
     @datasets[4].last << localmap_pose_opt
+    @datasets[5].last << relocate
+    @datasets[6].last << load_mapchunk
+    @datasets[7].last << misc
     @group_count += 1
 end
 
@@ -173,12 +187,14 @@ puts " ---------------------- Processing vtracker log --------------------------
     [:track_motion, []],
     [:update_localmap, []],
     [:search_mappoints, []],
-    [:localmap_pose_opt, []]
+    [:localmap_pose_opt, []],
+    [:relocate, []],
+    [:load_mapchunk, []],
+    [:misc, []],
 ]
 @labels = { }
 @group_count = 0
 @orb_costs = []
-@load_chunk_costs = []
 
 analyze_stream(parse_tokenize(@logFile))
 puts "   Analyzed #{@group_count} tracking activations"
@@ -195,6 +211,8 @@ puts "   Draw at most #{@options.max_num} activations"
 @chart.title_font_size = 32
 @chart.legend_font_size = 12
 @chart.marker_font_size = 12
+@chart.replace_colors(['#FDD84E', '#6886B4', '#72AE6E', '#D1695E', '#8A6EAF', '#EFAA43',
+                       '#FFE4E1', '#00FF00', 'white',])
 label_pos = 0
 (@options.start..[@group_count, @options.start + @options.max_num].min).each do | i |
     @chart.labels.merge!({ label_pos => @labels[i] })
@@ -206,6 +224,9 @@ end
 @chart.write "#{@logFile}.png"
 
 puts " ---------------------- Print other metrics ------------------------------- "
+[ ["ORB extract", @orb_costs], ["Load image frame", @datasets[0].last] ].each do | subject, data |
+    puts "   #{subject}: mean(#{data.mean.round(3)}ms), stddev(#{data.standard_deviation.round(3)}ms)"
+end
 
 puts " ---------------------- Finished ------------------------------------------ "
 system("xdg-open #{@logFile}.png")
