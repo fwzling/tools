065501c01aafd0383a6c21a338bec03942e1a53c
Feng Wei <wei.feng@uisee.com>
2018-12-18 09:44:53 +0800

    add frame_skip_det.rb
    Signed-off-by: Feng Wei <wei.feng@uisee.com>

---
+++ script/frame_skip_det.rb
@@ -0,0 +1,66 @@
+#!/usr/bin/env ruby
+
+## Description: Script to detect frame skip in dumped images under N frames threshold 
+#
+## Usage:       ruby frame_skip_det.rb [options] /path/to/dump_images/image/image_capturer_2
+
+require 'time'
+require 'optparse'
+require 'ostruct'
+
+# --- Define Command Line Options --- #
+
+def parse_options(args)
+    options = OpenStruct.new
+    options.verbose = false
+    options.max_skip = 10
+    options.dump_interval = 1
+
+    opt_parser = OptionParser.new do | opts |
+        opts.banner = "Usage:  frame_skip_det.rb  [options]  /path/to/dump_images/image/image_capturer_2"
+        opts.separator ""
+        opts.separator "Specific options:"
+
+        opts.on("--max_skip N", Integer, "The max threshold to accept normal for frame skip") do | n |
+            options.max_skip = n
+        end
+
+        opts.on("--dump_interval T", Integer, "Camera dump interval setting") do | t |
+            options.dump_interval = t
+        end
+
+        opts.on("--verbose", "Output trivial information") do
+            options.verbose = true
+        end
+    end
+
+    opt_parser.parse!(args)
+    options
+end
+
+# -------------------- main ---------------------- #
+
+FileExt = "tiff"
+@options = parse_options(ARGV)
+@imgDir = ARGV[0]
+
+exit 1 if @imgDir.nil?
+
+if not File.exist?(@imgDir)
+    puts "ERROR: #{@imgDir} not found"
+    exit 1
+end
+
+@imgFiles = Dir[@imgDir + "/*." + FileExt].sort
+
+@seqMarker = 0
+
+@imgFiles.each do | fn |
+    if matched = fn.match(/(\d+)\.\d+_(\d+)\.tiff/)
+        seq = matched[2].to_i
+        if seq - @seqMarker + @options.dump_interval > @options.max_skip
+            puts "Detected #{seq - @seqMarker + @options.dump_interval} frames skipped at #{fn}"
+        end
+        @seqMarker = seq
+    end
+end
